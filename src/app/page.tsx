08e583b88dd5ff3ab9f037021c7044b5bb456eb8
zizfn <1803942+zizifn@users.noreply.github.com>
2023-03-11 07:54:46 +0000

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
